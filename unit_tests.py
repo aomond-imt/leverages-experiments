dcf865ad14f012e7abc3eb3d11f48fa7759e3350
aomond <antoine.omond@imt-atlantique.fr>
2023-11-29 19:25:28 +0100

    Added unit test for expe topologies

--- unit_tests.py
+++ unit_tests.py
@@ -1,4 +1,5 @@
-from shared_methods import is_isolated_uptime
+from shared_methods import is_isolated_uptime, BANDWIDTH
+from topologies import star, chain, clique, ring, grid
 
 
 def test_is_isolated_uptime():
@@ -44,5 +45,60 @@ def test_is_isolated_uptime():
     print("test_is_isolated_uptime done")
 
 
+def test_star():
+    uptime_schedules = [[0], [30], [60]]
+    nodes_count = 3
+    tplgy, _ = star(nodes_count, BANDWIDTH)
+    expected_result = [False, False, True]
+    for node_num in range(nodes_count):
+        assert expected_result[node_num] == is_isolated_uptime(node_num, 0, uptime_schedules, nodes_count, tplgy)
+    print("test_star done")
+
+
+def test_chain():
+    uptime_schedules = [[0], [30], [75], [15]]
+    nodes_count = 4
+    tplgy, _ = chain(nodes_count, BANDWIDTH)
+    expected_result = [False, False, False, True]
+    for node_num in range(nodes_count):
+        assert expected_result[node_num] == is_isolated_uptime(node_num, 0, uptime_schedules, nodes_count, tplgy)
+    print("test_chain done")
+
+
+def test_grid():
+    uptime_schedules = [[0], [200], [0], [60], [200], [60], [0], [60], [0]]
+    nodes_count = 9
+    tplgy, _ = grid(nodes_count, BANDWIDTH)
+    expected_result = [True, False, True, True, False, True, True, True, True]
+    for node_num in range(nodes_count):
+        assert expected_result[node_num] == is_isolated_uptime(node_num, 0, uptime_schedules, nodes_count, tplgy)
+    print("test_grid done")
+
+
+def test_ring():
+    uptime_schedules = [[50], [80], [125], [65], [0]]
+    nodes_count = 5
+    tplgy, _ = ring(nodes_count, BANDWIDTH)
+    expected_result = [False, False, False, True, False]
+    for node_num in range(nodes_count):
+        assert expected_result[node_num] == is_isolated_uptime(node_num, 0, uptime_schedules, nodes_count, tplgy)
+    print("test_ring done")
+
+
+def test_clique():
+    uptime_schedules = [[50], [80], [125], [65], [0]]
+    nodes_count = 5
+    tplgy, _ = clique(nodes_count, BANDWIDTH)
+    expected_result = [False, False, False, False, False]
+    for node_num in range(nodes_count):
+        assert expected_result[node_num] == is_isolated_uptime(node_num, 0, uptime_schedules, nodes_count, tplgy)
+    print("test_clique done")
+
+
 if __name__ == "__main__":
     test_is_isolated_uptime()
+    test_star()
+    test_chain()
+    test_grid()
+    test_ring()
+    test_clique()
